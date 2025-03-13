e9fe4fb0bd4dcda01b33d0f9a8d751c6ed160384
Luis Garza <97556340+Luisg0527@users.noreply.github.com>
2025-03-12 22:10:30 -0600

    otro del gitignore

---
+++ Reto/Reto/Pages/CrearCuenta.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Reto.Pages;
+
+public class CrearCuentaModel : PageModel
+{
+    public int? empleadoId {get; set;}
+    public string? correo {get; set;}
+    public string? contraseña {get; set;}
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPostIniciarSesion() {
+        Response.Redirect("Index");
+    }
+}
