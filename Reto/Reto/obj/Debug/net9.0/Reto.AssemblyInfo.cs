39d537ef48cacbd8b866aa619af741fe26c02770
gpaolo00 <a01722922@tec.mx>
2025-03-12 21:53:05 -0600

    Otro Cambio Mas

--- Reto/Reto/obj/Debug/net9.0/Reto.AssemblyInfo.cs
+++ Reto/Reto/obj/Debug/net9.0/Reto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+597b0873bfe86790a4252506168842caf5fe051e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+700fc36a1e7f8d665f4b2ccf14d233c7fbfff115")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
