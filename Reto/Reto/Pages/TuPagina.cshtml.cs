6f0e78b915dd948dc99f73d5c533e3f1813d8ded
Santiago Paredes Suarez <santiparedes2004@gmail.com>
2025-03-24 23:08:22 -0600

    removi la pagina de paolo

---
+++ Reto/Reto/Pages/TuPagina.cshtml.cs
@@ -0,0 +1,16 @@
+using Reto.Model;
+using Reto.Pages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+
+
+public class TuPaginaModel : PageModel
+{
+
+
+    public void OnGet()
+    {
+
+    }
+}
--- Reto/Reto/obj/Debug/net9.0/Reto.AssemblyInfo.cs
+++ Reto/Reto/obj/Debug/net9.0/Reto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a92c3300eb045fea39501d4e2fc2139df1c2014")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceee2d5ce6526f392f5ff95f588b51c449da1b70")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
