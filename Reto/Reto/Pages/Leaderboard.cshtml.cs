1a5efbbabd5ece93900caca5ae9c814edb6e1d67
Santiago Paredes Suarez <santiparedes2004@gmail.com>
2025-03-24 23:01:26 -0600

    created santipa branch and page

---
+++ Reto/Reto/Pages/Leaderboard.cshtml.cs
@@ -0,0 +1,14 @@
+using Reto.Model;
+using Reto.Pages;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+
+
+public class LeaderboardModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
